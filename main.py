34ae6b6226ba54f5cb898d2cdee399178a637cf5
yaroshkin <yaroshkin255@ukr.net>
2023-06-30 22:21:51 +0300

    add visit users

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ app = Flask(__name__)
 bot_token = '6231927872:AAEJP7VLniSrRUJHA9g1U9deqxSkDJNUDzk'  # Замените на ваш токен бота
 bot = TeleBot(bot_token)
 
+visits = 0  # Переменная для хранения количества посещений
+
 def send_message_to_telegram(name, phone, email, message):
     chat_id = '-901537214'  # Замените на ваш Chat ID
     text = f'Имя: {name}\nТелефон: {phone}\nEmail: {email}\nСообщение: {message}'
     bot.send_message(chat_id, text)
 
-
 @app.route('/')
 def home():
-    return render_template('index.html')
+    global visits
+    visits += 1  # Увеличиваем счетчик посещений при каждом обращении к главной странице
+    return render_template('index.html', visits=visits)
 
 @app.route('/contact', methods=['POST'])
 def contact():
@@ -21,7 +24,6 @@ def contact():
     phone = request.form.get('phone')
     email = request.form.get('email')
     message = request.form.get('message')
-
 
     send_message_to_telegram(name, phone, email, message)
 
