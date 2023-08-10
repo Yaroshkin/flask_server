e1d4e93c78fe4e82a04e32fea45f2002c3ad7e0b
serg <yaroshkin255@ukr.net>
2023-08-10 20:41:40 +0300

    del email

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 bot_token = '6231927872:AAEJP7VLniSrRUJHA9g1U9deqxSkDJNUDzk'  # Замените на ваш токен бота
 bot = TeleBot(bot_token)
 
-def send_message_to_telegram(name, phone, email, message):
+def send_message_to_telegram(name, phone, message):
     chat_id = '-901537214'  # Замените на ваш Chat ID
-    text = f'Имя: {name}\nТелефон: {phone}\nEmail: {email}\nСообщение: {message}'
+    text = f'Имя: {name}\nТелефон: {phone}\nСообщение: {message}'
     bot.send_message(chat_id, text)
 
 
@@ -19,11 +19,11 @@ def home():
 def contact():
     name = request.form.get('name')
     phone = request.form.get('phone')
-    email = request.form.get('email')
+    # email = request.form.get('email')
     message = request.form.get('message')
 
 
-    send_message_to_telegram(name, phone, email, message)
+    send_message_to_telegram(name, phone, message)
 
     return jsonify({'success': True})
 
